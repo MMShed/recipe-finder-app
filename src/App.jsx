8ac845111d7bff76e4dcda0b3a33e83fa28bd70d
MMShed <88801787+MMShed@users.noreply.github.com>
2026-05-25 20:54:57 -0400

    Added filters, though not completely as I need to add uncheck ability

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import './styles.css'
 
 function App() {
 
+
+  const dietary_restrictions = ['vegetarian', 'vegan', 'gluten_free', 'dairy_free', 'nut_free', 'halal', 'kosher']
+  const cuisines = ['american', 'french', 'greek', 'italian', 'japanese', 'mexican', 'portuguese', 'spanish', 'thai', 'turkish']
+  const meal_types = ['starter', 'main', 'dessert', 'appetizer', 'breakfast', 'brunch', 'snack', 'side_dish', 'soup', 'drink', 'sauce']
+
+  const API_KEY = import.meta.env.VITE_API_KEY
+
   let [page, setPage] = useState("home")
   let [search, setSearch] = useState("")
   let [results, setResults] = useState([])
@@ -11,14 +18,51 @@ function App() {
   let [prevLink, setPrevLink] = useState(null)
   
   let [currRecipe, setCurrRecipe] = useState()
+  let [fetchURL, setFetchURL] = useState("")
 
-  const API_KEY = import.meta.env.VITE_API_KEY
+  const [showFilters, setShowFilters] = useState(false);
 
+  //Handle removing filters too!
+  const [filters, setFilters] = useState({
+    cuisine: [],
+    meal_type: [],
+    difficulty: [],
+    dietary_tags: [],
+    calories: { min: "", max: "" },
+    protein: { min: "", max: "" }
+  });
+
+  useEffect (() => {
+    for (const [key, value] of Object.entries(filters))
+    {
+      if (Array.isArray(value) && value.length > 0)
+      {
+        setFetchURL(prev => prev + `${key}=${value[0]}&`)
+      }
+    }
+
+  }, [filters])
 
   async function searchRecipe (query)
   {
+    //TODO: Handle query
+    let requestURL = `https://recipeapi.io/api/v1/recipes?`
+    
+
+    if (search !== "")
+    {
+      requestURL += `search=${query}&`
+    }
+
+    if (fetchURL !== "")
+    {
+      requestURL += fetchURL
+    }
+
+    if (requestURL[requestURL.length - 1] == '&') {requestURL.slice(0, -1)}
+
     const response = await fetch(
-    `https://recipeapi.io/api/v1/recipes?search=${query}`,
+    requestURL,
     {headers: {"Authorization": `Bearer ${API_KEY}`}}
     );
 
@@ -34,6 +78,8 @@ function App() {
 
       setNextLink(parsedInfo.links.next)
       setPrevLink(parsedInfo.links.prev)
+
+      if (parsedInfo.data.length == 0) {setPage("no recipes found")}
     }
         
   }
@@ -86,13 +132,85 @@ function App() {
   {
     return (
       <div className='appContainer'>
-        <h1>Welcome to the recipe searcher!</h1>
+        <h1>Welcome to Reci-Detector (Beta)</h1>
         <h4>Search up any recipes of your choice!</h4>
+        <h3>Use the filters to find recipes of your choice!</h3>
 
         <div className='searchBar'>
           <input type="text" onChange={(e) => setSearch(e.target.value)}></input>
           <button onClick={(e) => searchRecipe(search)}>Search Recipe</button>
         </div>
+
+
+        <button className='filter-btn' onClick={() => setShowFilters(prev => !prev)}>Filters</button>
+
+        {showFilters && (
+          <div className="filtersPanel">
+            <div className='Dietary Filters'>
+              
+              <h4>Dietary Restrictions</h4>
+
+              {
+                dietary_restrictions.map (restriction => {
+                  return (
+                    <div className='option'>
+                      <input type='radio' id={restriction} onChange={(e) => {
+
+                          setFilters({...filters, dietary_tags: [e.target.id]})
+
+                      }}></input>
+                      <label htmlFor={restriction}>{restriction.replaceAll("_", "-")}</label>
+                    </div>
+                  );
+                })
+              }
+            </div>
+
+            <div className='Cuisines'>
+              
+              <h4>Cuisine</h4>
+
+              {
+                cuisines.map (cuisine => {
+                  return (
+                    <div className='option'>
+                      <input type='radio' id={cuisine} onChange={(e) => {
+
+                        setFilters({...filters, cuisine: [e.target.id]})
+                      
+                      }}></input>
+                      <label htmlFor={cuisine}>{cuisine}</label>
+                    </div>
+                  );
+                })
+              }
+            </div>
+
+
+            
+            <div className='Meal Types'>
+              
+              <h4>Meal Types</h4>
+
+              {
+                meal_types.map (meal => {
+                  return (
+                    <div className='option'>
+                      <input type='radio' id={meal} onChange={(e) => {
+                        setFilters({...filters, meal: [e.target.id]})
+                      }}></input>
+                      <label htmlFor={meal}>{meal}</label>
+                    </div>
+                  );
+                })
+              }
+            </div>
+
+          </div>
+
+        )}
+  
+        
       </div>
     );
   }
@@ -189,6 +307,12 @@ function App() {
 
       </>
     )
+  }
+  else if (page == "no recipes found")
+  {
+    return <>
+      <h1>No recipes were found. Please try a different query.</h1>
+    </>
   }
 }
 
